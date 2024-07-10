5b8ca68483a86eefec6d3a9a6aee290651ded565
Luke Kucinski <lukek22b@gmail.com>
2024-07-10 12:24:32 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_105308) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "owner"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
