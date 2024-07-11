57d9bf612a29dbc5ec9bf6da38a0502b677cc9bb
Luke Kucinski <lukek22b@gmail.com>
2024-07-11 11:59:25 +0200

    reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :booking
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
-  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5), message: "%<value> is not a valid rating from 0 to 5" }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5) }
 end
