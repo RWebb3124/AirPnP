29b9e67a126576c0c5aa955de438eef3ad8cc69a
Ryan Webb <rwebb3124@gmail.com>
2024-07-11 16:00:29 +0100

    removed comment

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -12,7 +12,6 @@ class PoolsController < ApplicationController
 
   def show
     @pool = Pool.find(params[:id])
-    # The `geocoded` scope filters only flats with coordinates
     @marker = [lat: @pool.latitude, lng: @pool.longitude]
   end
 
